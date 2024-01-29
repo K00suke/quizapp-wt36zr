d6b882502a6641f4cd21f8add5bfb006fc5c21ce
K00suke <kosuke-kaide-2023@kenryo.ed.jp>
2024-01-29 17:38:14 +0900

    問題をオブジェクトで管理できるようにした。

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,35 @@
+<script>
+    import AnswerButton from "./AnswerButton.svelte";
+    
+    let currentScore = 0;
+    let renzokuSeikai = 0;
+    let quizData = {question: "この問題の正解はどれでしょう？", correctAnswer : ["O₂", "13.8"], taku: ["ア","O₂","①","doing","13.8"]};
+
+
+    const answerButtonClicked = (isCorrect) => {
+        if(isCorrect){
+            renzokuSeikai += 1;
+            currentScore += renzokuSeikai;
+        }
+        else{
+            renzokuSeikai = 0;
+        }
+    }
+</script>
 <main class="flex flex-col h-dvh">
     <!-- メニューバー -->
     <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:0</div>
-        <div>連続正解数:0</div>
+        <div>スコア:{currentScore}</div>
+        <div>連続正解数:{renzokuSeikai}</div>
     </div>
 
     <!-- 問題文 -->
-    <div class="bg-green-200 text-center text-4xl py-4">この問題の正解はどれでしょう？</div>
+    <div class="bg-green-200 text-center text-4xl py-4">{quizData.question}</div>
 
     <!-- 選択肢 -->
     <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">①</button>
-        <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">Textbook</button>
-        <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">13.8</button>
+        {#each quizData.taku as t}
+            <AnswerButton on:click={() => answerButtonClicked(quizData.correctAnswer.includes(t))}>{t}</AnswerButton>
+        {/each}
     </div>
 </main>
