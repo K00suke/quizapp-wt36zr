9ea6bf36800f2723d5f512fe86c1b8c2cd96d0fe
K00suke <kosuke-kaide-2023@kenryo.ed.jp>
2024-03-14 13:50:59 +0900

    Made the Title Page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
     import AnswerButton from "./AnswerButton.svelte";
+    import TitlePage from "./TitlePage.svelte";
     import {getQuizData} from "./quizdataFactory.js";
     
     let currentScore = 0;
     let renzokuSeikai = 0;
-    let quizData = {question: "この問題の正解はどれでしょう？", correctAnswer : "13.8", taku: ["ア","O₂","①","doing","13.8"]};
-
+    let quizData = getQuizData();
+    let showTitle = true;
 
     const answerButtonClicked = (isCorrect) => {
         if(isCorrect){
@@ -18,20 +19,24 @@
         quizData = getQuizData();
     }
 </script>
-<main class="flex flex-col h-dvh">
-    <!-- メニューバー -->
-    <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:{currentScore}</div>
-        <div>連続正解数:{renzokuSeikai}</div>
-    </div>
+{#if showTitle}
+    <TitlePage on:click={()=>showTitle=false}></TitlePage>
+{:else}
+    <main class="flex flex-col h-dvh">
+        <!-- メニューバー -->
+        <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+            <div>スコア:{currentScore}</div>
+            <div>連続正解数:{renzokuSeikai}</div>
+        </div>
 
-    <!-- 問題文 -->
-    <div class="bg-green-200 text-center text-4xl py-4">{quizData.question}</div>
+        <!-- 問題文 -->
+        <div class="bg-green-200 text-center text-4xl py-4">{quizData.question}</div>
 
-    <!-- 選択肢 -->
-    <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        {#each quizData.taku as t}
-            <AnswerButton on:click={() => answerButtonClicked(quizData.correctAnswer == t)}>{t}</AnswerButton>
-        {/each}
-    </div>
-</main>
+        <!-- 選択肢 -->
+        <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+            {#each quizData.taku as t}
+                <AnswerButton on:click={() => answerButtonClicked(quizData.correctAnswer == t)}>{t}</AnswerButton>
+            {/each}
+        </div>
+    </main>
+{/if}
---
+++ src/TitlePage.svelte
@@ -0,0 +1,11 @@
+<div class="bg-yellow-200 flex flex-col justify-center items-center h-dvh gap-16">
+    <div class="text-7xl font-extrabold">
+        Quiz App
+    </div>
+    <div class="text-2xl">
+        無意味なクイズ
+    </div>
+    <button on:click class="bg-blue-700 text-white font-bold rounded-full p-5 w-1/2">
+        スタート
+    </button>
+</div>
